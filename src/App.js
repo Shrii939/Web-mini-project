4188aad9f530ea1b4401ac6e7a2b17dfb4117773
Shrii939 <shridhars145@gmail.com>
2023-05-03 00:50:32 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import Login  from './components/Login';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom' ;
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-      <Routes>
-        <Route exact path="/" element={<Login/>} ></Route>
-      </Routes>
-     </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route path="/home" element={<Home/>}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
