d714bb0d7fecdc7820cd3f6d88fd040e45eac758
Shrii939 <shridhars145@gmail.com>
2023-05-14 22:41:44 +0530

    todo TextFile

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,20 +13,11 @@ const Header = (props) => {
           <div>
             <input type="text" placeholder="Search" />
           </div>
-        </Search>
+        
         <SearchIcon>
-        <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              data-supported-dps="16x16"
-              fill="currentColor"
-              width="16"
-              height="16"
-              focusable="false"
-            >
-              <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
-            </svg>
+          <img src="/images/search-icon.svg" alt="" />
         </SearchIcon>
+        </Search>
       </Content>
     </Container>
   );
@@ -52,9 +43,10 @@ const Content = styled.div`
 `;
 
 const Logo = styled.span`
-margin-right: 8px;
-font-size:0px
+  margin-right: 8px;
+  font-size: 0px;
 `;
+
 const Search = styled.div`
   opacity: 1;
   flex-grow: 1;
@@ -92,6 +84,5 @@ const SearchIcon = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
 
 export default Header;
